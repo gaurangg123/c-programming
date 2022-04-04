0deef48d939b86528a98db4222688dd4f39fecfd
gaurangg123 <gaurangashava@gmail.com>
2022-04-05 00:57:53 +0530

    initial commit

---
+++ Gender.c
@@ -0,0 +1,6 @@
+# include <stdio.h>
+int main (){
+    printf("my gender is M");
+return 0;
+
+}
---
+++ first.c
@@ -0,0 +1,7 @@
+# include <stdio.h>
+int main(){ 
+    
+    printf("Hello World I am Gaurang");
+    printf("I study in SRMIST, CHennai");
+    return 0;
+}
